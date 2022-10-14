f95f1baf24da104d809b7fa247783b78608522c9
gouthamjm <j.mgautham15@gmail.com>
2022-10-14 16:12:59 +0530

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import "tailwindcss/tailwind.css";
+
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(<div>Hello World</div>);
