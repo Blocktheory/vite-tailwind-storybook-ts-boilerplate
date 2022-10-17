c9e9dd7d613af39ea305ac9200e29c8624a89348
gouthamjm <j.mgautham15@gmail.com>
2022-10-17 10:41:02 +0530

    adding vitest

---
+++ src/add.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("addition", () => {
+    it("run", () => {
+        expect(1 + 2).toBe(3);
+    });
+});
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -3,31 +3,13 @@ import "./button.css";
 import React from "react";
 
 interface IButtonProps {
-    /**
-     * Is this the principal call to action on the page?
-     */
     primary?: boolean;
-    /**
-     * What background color to use
-     */
     backgroundColor?: string;
-    /**
-     * How large should the button be?
-     */
     size?: "small" | "medium" | "large";
-    /**
-     * Button contents
-     */
     label: string;
-    /**
-     * Optional click handler
-     */
     onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
     primary = false,
     size = "medium",
